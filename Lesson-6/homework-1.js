f1f71274b2f839515a125f487b6745c6d64ffc26
Yurii L <york.swanky@gmail.com>
2019-11-05 11:28:02 +0200

    while variation

--- Lesson-6/homework-1.js
+++ Lesson-6/homework-1.js
@@ -7,9 +7,15 @@
 var userIsHappy = true,
     currentNumber = 0;
 
-do {
+// do {
+//     userIsHappy =
+//         confirm ('Are you happy that your number is ' + currentNumber++ + '?');
+// } while (userIsHappy);
+
+// console.log('Thank you for game!');
+
+while (userIsHappy) {
     userIsHappy =
         confirm ('Are you happy that your number is ' + currentNumber++ + '?');
-} while (userIsHappy);
-
+}
 console.log('Thank you for game!');
